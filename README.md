81e45731f0984a3839a8a89bee520af234d44afd
Jacob R. Stevens <steven69@purdue.edu>
2016-12-01 01:42:06 -0800

    First trial on Ubuntu VM works

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Recommended Usage:
 5. Make necessary changes to Makefile and Makefile.config
 6. Run make-caffe.sh
 
+
 Necessary Changes To Caffe:
 + add the following lines to the Makefile:
     + CXXFLAGS += -Ipath/to/hdf5-1.8.18/src
     + CXXFLAGS += -Ipath/to/hdf5-1.8.18/hl/src
-    + CXXFLAGS += -Ipath/to/hdf5-1.10.0.patch/hl/src
 
 + make the following changes to Makefile.config:
     + uncomment CPU_ONLY := 1 (if using only CPU)
@@ -31,6 +31,7 @@ Dependencies:
 + libtool (Version 2.4.6)
 + automake (Version 1.15)
 + cmake (Version 3.7.0)
++ zlib (Version 1.2.8) *
 + boost (Version 1.60.0) *
 + atlas *
 + hdf5 *
@@ -40,9 +41,20 @@ Dependencies:
 + levelDB (Version 1.2) *
 + lmdb (Version 2.8) *
 + snappy (Version 1.1.3) *
+
+Note: the code that does a PKG_CHECK on snappy (inside configure file)
+has been removed. This is checking for gflags, which must be installed
+anyways
+
 Note: It is assumed that the dependencies marked with asteriks are NOT
 already installed. There is no check for the existence of these dependencies
 before install.
+
+Please note: this installer is intended for environments in which the
+installer has sudo permissions and Caffe is the sole intended application.
+It currently does NOT play nice with its installation of the asteriked
+dependencies and easily clobber existing installations of them. Would be a
+nice upgrade.
 
 Pull requests to this tool are kindly accepted. If you test on a new
 environment, please add it to the list.
--- make-caffe.sh
+++ make-caffe.sh
@@ -1,117 +1,164 @@
 #!/bin/bash
 
+# get m4, if necessary
+if (type m4 >/dev/null 2>&1) then
+    echo "m4 detected"
+else
+    echo "Installing m4"
+    cd m4-1.4.9
+    chmod +x configure
+    sudo ./configure; sudo make; sudo make install; 
+    echo "m4 installed"
+    cd ..
+fi
+
 # get autoreconf, if necessary
-if (type autoreconf 2>/dev/null) then
+if (type autoreconf >/dev/null 2>&1) then
     echo "autoreconf detected"
 else
     echo "Installing autoconf"
     cd autoconf-2.69
-    chmod configure
+    chmod +x configure
     ./configure
-    make; make install
+    sudo make; sudo make install
     echo "autoconf installed"
+    cd ..
 fi
-exit
+
+# get libtool, if necessary
+if (type libtool >/dev/null 2>&1) then
+    echo "libtool detected"
+else
+    echo "Installing libtool"
+    cd libtool-2.4.6
+    chmod +x configure
+    ./configure
+    sudo make; sudo make install
+    echo "libtool installed"
+    cd ..
+fi
+
+# get automake, if necessary
+if (type automake >/dev/null 2>&1) then
+    echo "automake detected"
+else
+    echo "Installing automake"
+    cd automake-1.15
+    chmod +x configure
+    ./configure
+    sudo make; sudo make install
+    echo "automake installed"
+    cd ..
+fi
+
+
+# get cmake, if necessary
+if (type cmake > /dev/null 2>&1) then
+    echo "cmake detected"
+else
+    echo "Installing cmake"
+    cd cmake-3.7.0
+    chmod +x bootstrap
+    ./bootstrap
+    sudo make
+    sudo make install
+    cd ..
+fi
+
 # Protobuf
 cd protobuf
-chmod +x autogen.sh
-ls
 ./autogen.sh
-chmod +x configure
-#./configure
-#make clean
-#make
-#make check
-#sudo make install
-#cd ..
-#
-##boost
-#cd boost_1_60_0 
-## Must force disable float 128 support
-## by removing #ifdef/#endif around BOOST_MATH_DISABLE_FLOAT128 in
-## boost/math/tools/config.hpp
-#./bootstrap.sh
-#./bjam install
-#cd ..
-#
-## glog
-#cd glog
-#chmod +x configure
-#./configure
-#./make
-#./sudo make install
-#cd ..
-#
-## cmake
-#cd cmake-3.7.0
-#chmod +x bootstrap
-#./bootstrap
-#make
-#make install
-#cd ..
-#
-## gflags
-#cd gflags
-#mkdir build
-#cd build
-#cmake ..
-#make
-#make install
-#cd ..
-#
-## ATLAS
-#cd ATLAS
-#chmod +x configure
-#mkdir build
-#cd build
-## cripple-atlas-performance necessary for because of Atom
-#../configure --shared -b 32 --nof77 --cripple-atlas-performance
-#make
-#make install
-#cp lib/*.so /usr/local/lib
-#cp lib/*.a /usr/local/lib
-#cd ..
-#mkdir /usr/local/include/atlas
-#cp -r include/* /usr/local/include/atlas
-#cd ..
-#
-## hdf5
-#cd hdf5-1.8.18 
-#./configure --prefix=/usr/local/hdf5
-#make
-#make install
-#cp hl/src/.libs/*.a /usr/local/lib
-#cp h1/src/.libs/*.so /usr/local/lib
-#cp src/.libs/*.a /usr/local/lib
-#cp src/.libs/*.so /usr/local/lib
-#cd ..
-#
-## LevelDB
-#cd leveldb
-#make
-#sudo cp out-shared/libleveldb.* /usr/local/lib
-#cd include
-#sudo cp -R leveldb /usr/local/include
-#cd ../..
-#
-## LMDB
-#cd lmdb/libraries/liblmdb
-#make
-#make install
-#cd ../../..
-#
-## Snappy
-#cd snappy
-#chmod +x ./autogen.sh
-#./autogen.sh
-#chmod +x ./configure
-#./configure
-#make; make install
-#cd ..
-#
-## Caffe
-#cd caffe
-#make all
-#make test
-#make runtest
-#cd ..
+./configure
+make clean
+make
+make check
+sudo make install
+cd ..
+exit
+
+# boost
+cd boost_1_60_0 
+# Must force disable float 128 support
+# by removing #ifdef/#endif around BOOST_MATH_DISABLE_FLOAT128 in
+# boost/math/tools/config.hpp
+./bootstrap.sh
+sudo ./bjam install
+cd ..
+
+# glog
+cd glog
+mkdir build
+cd build
+export CXXFLAGS="-fPIC"
+sudo cmake -DBUILD_SHARED_LIBS=true .. 
+make
+sudo make install
+cd ../..
+
+# gflags
+cd gflags
+mkdir build
+cd build
+export CXXFLAGS="-fPIC"
+sudo cmake -DBUILD_SHARED_LIBS=true ..
+make
+sudo make install
+cd ../..
+
+# ATLAS
+cd ATLAS
+mkdir build
+cd build
+# cripple-atlas-performance necessary for because of Atom
+# change 32 to reflect specific machine (aka, 32 or 64)
+../configure --shared --dylibs -b 64 --nof77 --cripple-atlas-performance
+make
+sudo make install
+sudo cp lib/*.so /usr/local/lib
+sudo cp lib/*.a /usr/local/lib
+cd ..
+sudo mkdir /usr/local/include/atlas
+sudo cp -r include/* /usr/local/include/atlas
+cd ..
+
+
+# zlib
+cd zlib-1.2.8
+./configure
+make
+sudo make install
+cd ..
+ 
+# hdf5
+cd hdf5-1.10.0-patch1 
+./configure --enable-cxx --prefix=/usr/local/
+make
+sudo make install
+cd ..
+
+# LevelDB
+cd leveldb
+make
+sudo cp out-shared/libleveldb.* /usr/local/lib
+cd include
+sudo cp -R leveldb /usr/local/include
+cd ../..
+
+# LMDB
+cd lmdb/libraries/liblmdb
+make
+sudo make install
+cd ../../..
+
+# Snappy
+cd snappy-1.1.3
+./configure
+make; sudo make install
+cd ..
+
+# Caffe
+cd caffe
+make all
+make test
+make runtest
+cd ..
